ac459fbc2218479667e8b1105d69d4a8c71f30fa
fingerpich <zarei.bs@gmail.com>
2022-03-09 12:23:15 +0330

    added named css colors

--- action-on-page/fetch-colors.js
+++ action-on-page/fetch-colors.js
@@ -1,13 +1,26 @@
 import {runFunctionInPage} from './extension.js'
+import { namedColors } from '../color.js'
 
 export async function listColors() {
   let uniqueSortedColors;
-  return runFunctionInPage(findColorsInStyle).then((colorsList) => {
-    const colorUsage = colorsList.reduce((obj, color) => {
-      if (!obj[color]) { obj[color] = 1}
-      else { obj[color]++ }
-      return obj
-    }, {})
+  //[\\s;!,}]
+  // const hexRegex = '#(([0-9a-fA-F]{2}){3,4}|([0-9a-fA-F]){3,4})'
+  const hexRegex = '#[0-9a-fA-F]{3,8}'
+  const rgbaRegex = 'rgba?\\([\\d,\\.\\s]*\\)'
+  const hslaRegex = 'hsla?\\([\\d,%\\.\\s]*\\)'
+  const colorNamesRegex = Object
+    .keys(namedColors)
+    .map(name => name+'[\\s;!,}]')
+    .join('|')
+  const regexText = `${hexRegex}|${rgbaRegex}|${hslaRegex}|${colorNamesRegex}`
+  return runFunctionInPage(findColorsInStyle, [regexText]).then((colorsList) => {
+    const colorUsage = colorsList
+      .map(color => color.replace(/[\s;!,}]/g, ''))
+      .reduce((obj, color) => {
+        if (!obj[color]) { obj[color] = 1}
+        else { obj[color]++ }
+        return obj
+      }, {})
 
     // sort colors by the frequent
     uniqueSortedColors = Object
@@ -17,14 +30,14 @@ export async function listColors() {
   })
 }
 
-function findColorsInStyle() {
+function findColorsInStyle(regexText) {
   const styles = document.getElementsByTagName('style')
+  const reg = new RegExp(regexText,'ig')
   const colorsList = Array.from(styles).reduce((list, style) => {
-    // const reg = /#(([0-9a-fA-F]{2}){3,4}|([0-9a-fA-F]){3,4})/g
-    const reg = /(#(([0-9a-fA-F]{2}){3,4}|([0-9a-fA-F]){3,4}))|rgba?\([\d,\.\s]*\)|hsla\([\d,\%\.\s]*\)/g
     const colors = style.innerHTML.match(reg)
     return [...list, ...(colors || [])]
   }, [])
 
   return colorsList
 }
+
